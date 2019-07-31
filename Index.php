8bb815ff29a0fa6afb089ba233cffe60a077b1ff
Nguyen Thang <nguyenthang011099.hust@gmail.com>
2019-07-31 17:17:51 +0700

    fix VN

--- Index.php
+++ Index.php
@@ -1,32 +1,36 @@
 <?php
 require 'vendor/autoload.php';
-class Index{
-    public $faker ;
+
+class Index
+{
+    public $faker;
     public $number;
 
-    public  function __construct($number)
+    public function __construct($number)
     {
-        $this->number= $number;
-        $this->faker = Faker\Factory::create();
+        $this->number = $number;
+        $this->faker = Faker\Factory::create('vi_VN');
 
     }
 
-    public function information(){
+    public function information()
+    {
         $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        for ($i = 0; $i < $this->number; $i++) {
-            $txt[]= $this->faker->name . "\n";
-            $txt[]= $this->faker->phoneNumber."\n";
-            $txt[]= $this->faker->address . "\n";
-            $txt[]= "========================="."\n";
-            foreach ($txt as $txt1){
+
+        for ($i = 0; $i < $this->number; ++$i) {
+            $txt = [];
+            $txt[] = $this->faker->name . "\n";
+            $txt[] = $this->faker->phoneNumber . "\n";
+            $txt[] = $this->faker->address . "\n";
+            $txt[] = "=========================" . "\n";
+            foreach ($txt as $txt1) {
                 fwrite($myfile, $txt1);
             }
 
         }
         fclose($myfile);
-
     }
 }
 
-$file= new Index(4);
+$file = new Index(3);
 $file->information();
